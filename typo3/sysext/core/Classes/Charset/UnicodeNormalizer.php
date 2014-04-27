f6e411103a6a3b72ed649186ecd7d5c3fb2cb90e
Stephan Jorek <stephan.jorek@artplan21.de>
2014-04-27 15:24:10 +0200

    updated unicode normalizer to reflect previously commited feature-configuration implementation

--- typo3/sysext/core/Classes/Charset/UnicodeNormalizer.php
+++ typo3/sysext/core/Classes/Charset/UnicodeNormalizer.php
@@ -24,14 +24,6 @@ namespace TYPO3\CMS\Core\Charset;
  *  This copyright notice MUST APPEAR in all copies of the script!
  ***************************************************************/
 
-if (!extension_loaded('intl')) {
-	throw new \TYPO3\CMS\Core\Exception('Missing php\'s “intl” extension.', 1398597475);
-}
-
-if (!class_exists('\\Normalizer', false)) {
-	throw new \TYPO3\CMS\Core\Exception('Missing unicode normalization implementation class.', 1398597477);
-}
-
 /**
  * Class for normalizing unicode. This implementation is a simple facade to “php-intl” extension's “Normalizer”-class.
  *
@@ -44,40 +36,131 @@ if (!class_exists('\\Normalizer', false)) {
 class UnicodeNormalizer {
 
 	/**
+	 * No decomposition/composition
+	 */
+	const NONE = 1;
+
+	/**
+	 * Normalization Form C (NFC) - Canonical Decomposition followed by Canonical Composition
+	 */
+	const FORM_C = 4;
+
+	/**
+	 * Normalization Form C (NFC) - Canonical Decomposition followed by Canonical Composition
+	 */
+	const NFC = 4;
+
+	/**
+	 * Normalization Form KC (NFKC) - Compatibility Decomposition, followed by Canonical Composition
+	 */
+	const FORM_KC = 5;
+
+	/**
+	 * Normalization Form KC (NFKC) - Compatibility Decomposition, followed by Canonical Composition
+	 */
+	const NFKC = 5;
+
+	/**
+	 * Normalization Form D (NFD) - Canonical Decomposition
+	 */
+	const FORM_D = 2;
+
+	/**
+	 * Normalization Form D (NFD) - Canonical Decomposition
+	 */
+	const NFD = 2;
+
+	/**
+	 * Normalization Form KD (NFKD) - Compatibility Decomposition
+	 */
+	const FORM_KD = 3;
+
+	/**
+	 * Normalization Form KD (NFKD) - Compatibility Decomposition
+	 */
+	const NFKD = 3;
+
+	/**
 	 * A string indicating which unicode normalization form to use. Must be set to one of the following constants:
 	 * \Normalizer::FORM_C, \Normalizer::FORM_D, \Normalizer::FORM_KC, \Normalizer::FORM_KD or \Normalizer::NONE
 	 *
-	 * @var string
+	 * @var integer
 	 */
-	protected $form = NULL;
+	protected $normalizationForm = NULL;
 
 	/**
-	 * @param string $form Set the default normalization form to one of the available constants; \Normalizer::NONE is default.
+	 * A falg indicating which normalizer to use - TRUE means use intl's Normalizer, FALSE or NULL
+	 *
+	 * @var boolean
+	 */
+	protected $useIntlNormalizer = NULL;
+
+	/**
+	 * @param integer $normalizationForm Set the default normalization form to one of the available constants; \Normalizer::NONE is default.
 	 * @see http://www.php.net/manual/en/class.normalizer.php
 	 */
-	public function __construct($form = NULL) {
-		$this->form = $form ?: \Normalizer::NONE;
+	public function __construct($normalizationForm = NULL) {
+		$this->useIntlNormalizer = (boolean) ($GLOBALS['TYPO3_CONF_VARS']['SYS']['unicodeNormalizer'] === 'intl');
+
+		if ($normalizationForm === NULL) {
+			$normalizationForm = $this->useIntlNormalizer ? \Normalizer::NONE : self::NONE;
+		}
+		$this->setNormalizationForm($normalizationForm);
 	}
 
 	/**
-	 * Checks if the provided string is already in the specified or current default normalization form.
+	 * Checks if the provided integer is already in the specified or current default normalization form.
 	 *
+	 * @link http://www.php.net/manual/en/normalizer.isnormalized.php
 	 * @param string $input The string to check.
-	 * @param string $form An optional normalization form to check against, overriding the default; see constructor.
+	 * @param integer $normalizationForm An optional normalization form to check against, overriding the default; see constructor.
 	 * @return boolean TRUE if normalized, FALSE otherwise or if an error occurred.
+	 * @throws \TYPO3\CMS\Core\Charset\Exception\NotImplementedException
 	 */
-	public function isNormalized($input, $form = NULL) {
-		return \Normalizer::isNormalized($input, $form ?: $this->form);
+	public function isNormalized($input, $normalizationForm = NULL) {
+		if ($this->useIntlNormalizer) {
+			return \Normalizer::isNormalized($input, $normalizationForm ?: $this->normalizationForm);
+		}
+		throw new Exception\NotImplementedException();
 	}
 
 	/**
 	 * Normalizes the input provided and returns the normalized string.
 	 *
+	 * @link http://www.php.net/manual/en/normalizer.normalize.php
 	 * @param string $input The string to normalize.
-	 * @param string $form An optional normalization form to check against, overriding the default; see constructor.
+	 * @param integer $normalizationForm An optional normalization form to check against, overriding the default; see constructor.
 	 * @return string|NULL The normalized string or NULL if an error occurred.
+	 * @throws \TYPO3\CMS\Core\Charset\Exception\NotImplementedException
 	 */
-	public function normalize($input, $form = NULL) {
-		return \Normalizer::normalize($input, $form ?: $this->form);
+	public function normalize($input, $normalizationForm = NULL) {
+		if ($this->useIntlNormalizer) {
+			return \Normalizer::normalize($input, $normalizationForm ?: $this->normalizationForm);
+		}
+		throw new Exception\NotImplementedException();
+	}
+
+	/**
+	 * Set the current normalization form constant.
+	 *
+	 * @param integer $normalizationForm
+	 * @return void
+	 */
+	public function setNormalizationForm($normalizationForm) {
+		$normalizationForms = $this->useIntlNormalizer ? array(\Normalizer::NONE, \Normalizer::FORM_C, \Normalizer::FORM_D, \Normalizer::FORM_KC, \Normalizer::FORM_KD)
+		                                               : array(self::NONE, self::FORM_C, self::FORM_D, self::FORM_KC, self::FORM_KD);
+		if (in_array($normalizationForm, $normalizationForms)) {
+			$this->normalizationForm = (integer) $normalizationForm;
+		}
+		throw new \InvalidArgumentException(sprintf('Unknown normalization form constant “%s”.', $normalizationForm), 1398603947);
+	}
+
+	/**
+	 * Retrieve the current normalization form constant.
+	 *
+	 * @return integer
+	 */
+	public function getNormalizationForm() {
+		return $this->normalizationForm;
 	}
 }
