3298f8a52b5deffefc2671bb1adc162e74a15343
Stephan Jorek <stephan.jorek@artplan21.de>
2014-05-10 13:41:27 +0200

    implemented 4 “UnicodeNormalizer::isNormalized” checks

--- typo3/sysext/core/Tests/Unit/Charset/UnicodeNormalizerTest.php
+++ typo3/sysext/core/Tests/Unit/Charset/UnicodeNormalizerTest.php
@@ -24,6 +24,8 @@ namespace TYPO3\CMS\Core\Tests\Unit\Charset;
  *  This copyright notice MUST APPEAR in all copies of the script!
  ***************************************************************/
 
+use \TYPO3\CMS\Core\Charset\UnicodeNormalizer;
+
 /**
  * Testcase for \TYPO3\CMS\Core\Charset\UnicodeNormalizer
  *
@@ -37,23 +39,85 @@ class UnicodeNormalizerTest extends \TYPO3\CMS\Core\Tests\UnitTestCase {
 	protected $fixture = NULL;
 
 	public function setUp() {
-		$this->fixture = new \TYPO3\CMS\Core\Charset\UnicodeNormalizer();
+		$this->fixture = new UnicodeNormalizer();
 	}
 
 	///////////////////////////////////
 	// Tests concerning isNormalized
 	///////////////////////////////////
+
 	/**
+	 * Check if normalization-detection works for pure ascii strings
+	 *
 	 * @test
 	 * @see http://forge.typo3.org/issues/57695
 	 */
-	public function stringIsNormalized() {
-		$this->fail('missing unit-test implementation');
+	public function checkAsciiStringIsNormalized() {
+
+		$ascii_dejavu = 'dejavu';
+
+		$this->assertFalse($this->fixture->isNormalized($dejavu, UnicodeNormalizer::NONE));
+		$this->assertTrue($this->fixture->isNormalized($dejavu, UnicodeNormalizer::FORM_D));
+		$this->assertTrue($this->fixture->isNormalized($dejavu, UnicodeNormalizer::FORM_KD));
+		$this->assertTrue($this->fixture->isNormalized($dejavu, UnicodeNormalizer::FORM_C));
+		$this->assertTrue($this->fixture->isNormalized($dejavu, UnicodeNormalizer::FORM_KC));
+	}
+
+	/**
+	 * Check if normalization-detection works for pre-composed unicode-strings (only utf8)
+	 *
+	 * @test
+	 * @see http://forge.typo3.org/issues/57695
+	 */
+	public function checkPrecomposedStringIsNormalized() {
+		// fantasy-string: déjàvü
+		$nfc_dejavu = hex2bin('64c3a96ac3a076c3bc');
+
+		$this->assertFalse($this->fixture->isNormalized($nfc_dejavu, UnicodeNormalizer::NONE));
+		$this->assertFalse($this->fixture->isNormalized($nfc_dejavu, UnicodeNormalizer::FORM_D));
+		$this->assertFalse($this->fixture->isNormalized($nfc_dejavu, UnicodeNormalizer::FORM_KD));
+		$this->assertTrue($this->fixture->isNormalized($nfc_dejavu, UnicodeNormalizer::FORM_C));
+		$this->assertTrue($this->fixture->isNormalized($nfc_dejavu, UnicodeNormalizer::FORM_KC));
+	}
+
+	/**
+	 * Check if normalization-detection works for decomposed unicode-strings (only utf8)
+	 *
+	 * @test
+	 * @see http://forge.typo3.org/issues/57695
+	 */
+	public function checkDecomposedStringIsNormalized() {
+		// the same string as above, but decomposed
+		$nfd_dejavu = hex2bin('6465cc816a61cc807675cc88');
+
+		$this->assertFalse($this->fixture->isNormalized($nfd_dejavu, UnicodeNormalizer::NONE));
+		$this->assertTrue($this->fixture->isNormalized($nfd_dejavu, UnicodeNormalizer::FORM_D));
+		$this->assertTrue($this->fixture->isNormalized($nfd_dejavu, UnicodeNormalizer::FORM_KD));
+		$this->assertFalse($this->fixture->isNormalized($nfd_dejavu, UnicodeNormalizer::FORM_C));
+		$this->assertFalse($this->fixture->isNormalized($nfd_dejavu, UnicodeNormalizer::FORM_KC));
+	}
+
+	/**
+	 * Check if normalization-detection works for combined pre- and decomposed unicode-strings (only utf8)
+	 *
+	 * @test
+	 * @see http://forge.typo3.org/issues/57695
+	 */
+	public function checkCombinedStringIsNormalized() {
+		// combination of all three strings from above
+		$ascii_nfc_nfd_dejavu = 'dejavu'.hex2bin('64c3a96ac3a076c3bc').hex2bin('6465cc816a61cc807675cc88');
+
+		$this->assertFalse($this->fixture->isNormalized($ascii_nfc_nfd_dejavu, UnicodeNormalizer::NONE));
+		$this->assertFalse($this->fixture->isNormalized($ascii_nfc_nfd_dejavu, UnicodeNormalizer::FORM_D));
+		$this->assertFalse($this->fixture->isNormalized($ascii_nfc_nfd_dejavu, UnicodeNormalizer::FORM_KD));
+		$this->assertFalse($this->fixture->isNormalized($ascii_nfc_nfd_dejavu, UnicodeNormalizer::FORM_C));
+		$this->assertFalse($this->fixture->isNormalized($ascii_nfc_nfd_dejavu, UnicodeNormalizer::FORM_KC));
 	}
 
 	///////////////////////////////////
 	// Tests concerning normalize
 	///////////////////////////////////
+
 	/**
 	 * @test
 	 * @see http://forge.typo3.org/issues/57695
