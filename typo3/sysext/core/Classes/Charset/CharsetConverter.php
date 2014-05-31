a66a19a1f6fbcf7e9cacc61faf8e45c628fd0a41
Stephan Jorek <stephan.jorek@artplan21.de>
2014-05-31 16:25:27 +0200

    updated and added TODO tags

--- typo3/sysext/core/Classes/Charset/CharsetConverter.php
+++ typo3/sysext/core/Classes/Charset/CharsetConverter.php
@@ -695,6 +695,7 @@ class CharsetConverter {
 	 * @todo Define visibility
 	 */
 	public function conv($str, $fromCS, $toCS, $useEntityForNoChar = 0) {
+		// TODO Figure out if we need/want unicode-normalization as well …
 		if ($fromCS == $toCS) {
 			return $str;
 		}
@@ -744,6 +745,7 @@ class CharsetConverter {
 	 * @todo Define visibility
 	 */
 	public function convArray(&$array, $fromCS, $toCS, $useEntityForNoChar = 0) {
+		// TODO Figure out if we need/want unicode-normalization as well …
 		foreach ($array as $key => $value) {
 			if (is_array($array[$key])) {
 				$this->convArray($array[$key], $fromCS, $toCS, $useEntityForNoChar);
