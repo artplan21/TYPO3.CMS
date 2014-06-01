1df0023b80181e75351aec837269cd8f84ec4cbc
Stephan Jorek <stephan.jorek@artplan21.de>
2014-06-01 14:47:21 +0200

    UnicodeNormalizerTest: switched from several combined assertions per tests to one assertion per test using the @dataProvider annotation

--- typo3/sysext/core/Tests/Unit/Charset/UnicodeNormalizerTest.php
+++ typo3/sysext/core/Tests/Unit/Charset/UnicodeNormalizerTest.php
@@ -47,71 +47,116 @@ class UnicodeNormalizerTest extends \TYPO3\CMS\Core\Tests\UnitTestCase {
 	///////////////////////////////////
 
 	/**
-	 * Check if normalization-detection works for pure ascii strings
+	 * DataProvider for test: checkIfStringIsNormalized
 	 *
-	 * @test
-	 * @see http://forge.typo3.org/issues/57695
+	 * Provides the following types of strings:
+	 *
+	 * - pure ASCII strings
+	 * - UTF8 encoded strings, normalized to …
+	 *   - … NFC: pre-composed unicode-strings
+	 *   - … NFD: decomposed unicode-strings
+	 * - combination of all string-types from above
+	 *
+	 * @return array<array<boolean|string|integer>>
 	 */
-	public function checkIfAsciiStringIsNormalized() {
-
+	public function checkIfStringIsNormalizedDataProvider() {
 		$ascii_dejavu = 'dejavu';
-
-		$this->assertFalse($this->fixture->isNormalized($ascii_dejavu, UnicodeNormalizer::NONE));
-		$this->assertTrue($this->fixture->isNormalized($ascii_dejavu, UnicodeNormalizer::FORM_D));
-		$this->assertTrue($this->fixture->isNormalized($ascii_dejavu, UnicodeNormalizer::FORM_KD));
-		$this->assertTrue($this->fixture->isNormalized($ascii_dejavu, UnicodeNormalizer::FORM_C));
-		$this->assertTrue($this->fixture->isNormalized($ascii_dejavu, UnicodeNormalizer::FORM_KC));
-	}
-
-	/**
-	 * Check if normalization-detection works for pre-composed unicode-strings (only utf8)
-	 *
-	 * @test
-	 * @see http://forge.typo3.org/issues/57695
-	 */
-	public function checkIfPrecomposedStringIsNormalized() {
 		// fantasy-string: déjàvü
 		$nfc_dejavu = hex2bin('64c3a96ac3a076c3bc');
-
-		$this->assertFalse($this->fixture->isNormalized($nfc_dejavu, UnicodeNormalizer::NONE));
-		$this->assertFalse($this->fixture->isNormalized($nfc_dejavu, UnicodeNormalizer::FORM_D));
-		$this->assertFalse($this->fixture->isNormalized($nfc_dejavu, UnicodeNormalizer::FORM_KD));
-		$this->assertTrue($this->fixture->isNormalized($nfc_dejavu, UnicodeNormalizer::FORM_C));
-		$this->assertTrue($this->fixture->isNormalized($nfc_dejavu, UnicodeNormalizer::FORM_KC));
-	}
-
-	/**
-	 * Check if normalization-detection works for decomposed unicode-strings (only utf8)
-	 *
-	 * @test
-	 * @see http://forge.typo3.org/issues/57695
-	 */
-	public function checkIfDecomposedStringIsNormalized() {
 		// the same string as above, but decomposed
 		$nfd_dejavu = hex2bin('6465cc816a61cc807675cc88');
+		// combination of all three strings from above
+		$ascii_nfc_nfd_dejavu = $ascii_dejavu.$nfc_dejavu.$nfd_dejavu;
 
-		$this->assertFalse($this->fixture->isNormalized($nfd_dejavu, UnicodeNormalizer::NONE));
-		$this->assertTrue($this->fixture->isNormalized($nfd_dejavu, UnicodeNormalizer::FORM_D));
-		$this->assertTrue($this->fixture->isNormalized($nfd_dejavu, UnicodeNormalizer::FORM_KD));
-		$this->assertFalse($this->fixture->isNormalized($nfd_dejavu, UnicodeNormalizer::FORM_C));
-		$this->assertFalse($this->fixture->isNormalized($nfd_dejavu, UnicodeNormalizer::FORM_KC));
+		return array(
+			// pure ASCII string
+			'ASCII - is not normalized if form is NONE' => array(
+				FALSE, $ascii_dejavu, UnicodeNormalizer::NONE
+			),
+			'ASCII - is in normalization-form D' => array(
+				TRUE, $ascii_dejavu, UnicodeNormalizer::NFD
+			),
+			'ASCII - is in normalization-form DK' => array(
+				TRUE, $ascii_dejavu, UnicodeNormalizer::NFKD
+			),
+			'ASCII - is in normalization-form C' => array(
+				TRUE, $ascii_dejavu, UnicodeNormalizer::NFC
+			),
+			'ASCII - is in normalization-form KC' => array(
+				TRUE, $ascii_dejavu, UnicodeNormalizer::NFKC
+			),
+			// unicode string in normalization-form C
+			'NFC - is not normalized if form is NONE' => array(
+				FALSE, $nfc_dejavu, UnicodeNormalizer::NONE
+			),
+			'NFC - is not in normalization-form D' => array(
+				FALSE, $nfc_dejavu, UnicodeNormalizer::NFD
+			),
+			'NFC - is not in normalization-form DK' => array(
+				FALSE, $nfc_dejavu, UnicodeNormalizer::NFKD
+			),
+			'NFC - is in normalization-form C' => array(
+				TRUE, $nfc_dejavu, UnicodeNormalizer::NFC
+			),
+			'NFC - may be in normalization-form KC' => array(
+				TRUE, $nfc_dejavu, UnicodeNormalizer::NFKC
+			),
+			// unicode string in normalization-form D
+			'NFD - is not normalized if form is NONE' => array(
+				FALSE, $nfd_dejavu, UnicodeNormalizer::NONE
+			),
+			'NFD - is in normalization-form D' => array(
+				TRUE, $nfd_dejavu, UnicodeNormalizer::NFD
+			),
+			'NFD - may be in normalization-form DK' => array(
+				TRUE, $nfd_dejavu, UnicodeNormalizer::NFKD
+			),
+			'NFD - is not in normalization-form C' => array(
+				FALSE, $nfd_dejavu, UnicodeNormalizer::NFC
+			),
+			'NFD - is not in normalization-form KC' => array(
+				FALSE, $nfd_dejavu, UnicodeNormalizer::NFKC
+			),
+			// combination of ascii and unicode strings in normalization-forms C and D
+			'ASCII + NFC + NFD - is not normalized if form is NONE' => array(
+				FALSE, $ascii_nfc_nfd_dejavu, UnicodeNormalizer::NONE
+			),
+			'ASCII + NFC + NFD - is not in normalization-form D' => array(
+				FALSE, $ascii_nfc_nfd_dejavu, UnicodeNormalizer::NFD
+			),
+			'ASCII + NFC + NFD - is not in normalization-form DK' => array(
+				FALSE, $ascii_nfc_nfd_dejavu, UnicodeNormalizer::NFKD
+			),
+			'ASCII + NFC + NFD - is not in normalization-form D' => array(
+				FALSE, $ascii_nfc_nfd_dejavu, UnicodeNormalizer::NFC
+			),
+			'ASCII + NFC + NFD - is not in normalization-form KC' => array(
+				FALSE, $ascii_nfc_nfd_dejavu, UnicodeNormalizer::NFKC
+			),
+		);
 	}
 
 	/**
-	 * Check if normalization-detection works for combined ascii, pre- and decomposed unicode-strings (only utf8)
+	 * Check if unicode-normalization-detection works for the provided types of strings
 	 *
 	 * @test
+	 * @dataProvider checkIfStringIsNormalizedDataProvider
 	 * @see http://forge.typo3.org/issues/57695
+	 *
+	 * @param boolean $expectedResult
+	 * @param string $testString
+	 * @param integer $normalizationForm
+	 * @return void
 	 */
-	public function checkIfCombinedStringIsNormalized() {
-		// combination of all three strings from above
-		$ascii_nfc_nfd_dejavu = 'dejavu'.hex2bin('64c3a96ac3a076c3bc').hex2bin('6465cc816a61cc807675cc88');
-
-		$this->assertFalse($this->fixture->isNormalized($ascii_nfc_nfd_dejavu, UnicodeNormalizer::NONE));
-		$this->assertFalse($this->fixture->isNormalized($ascii_nfc_nfd_dejavu, UnicodeNormalizer::FORM_D));
-		$this->assertFalse($this->fixture->isNormalized($ascii_nfc_nfd_dejavu, UnicodeNormalizer::FORM_KD));
-		$this->assertFalse($this->fixture->isNormalized($ascii_nfc_nfd_dejavu, UnicodeNormalizer::FORM_C));
-		$this->assertFalse($this->fixture->isNormalized($ascii_nfc_nfd_dejavu, UnicodeNormalizer::FORM_KC));
+	public function checkIfStringIsNormalized($expectedResult, $testString, $normalizationForm) {
+		$actualResult = $this->fixture->isNormalized($testString, $normalizationForm);
+		if ($expectedResult === TRUE) {
+			$this->assertTrue($actualResult);
+		} elseif ($expectedResult === FALSE) {
+			$this->assertFalse($actualResult);
+		} else {
+			$this->assertSame($expectedResult, $actualResult);
+		}
 	}
 
 	///////////////////////////////////
@@ -119,72 +164,120 @@ class UnicodeNormalizerTest extends \TYPO3\CMS\Core\Tests\UnitTestCase {
 	///////////////////////////////////
 
 	/**
-	 * Check if normalization works for pure ascii strings
+	 * DataProvider for test: checkStringNormalization
 	 *
-	 * @test
-	 * @see http://forge.typo3.org/issues/57695
+	 * Provides the following types of strings:
+	 *
+	 * - pure ASCII strings
+	 * - UTF8 encoded strings,not …
+	 *   - … NFC: pre-composed unicode-strings
+	 *   - … NFD: decomposed unicode-strings
+	 * - combination of all string-types from above
+	 *
+	 * @return array<array<boolean|string|integer>>
 	 */
-	public function checkAsciiStringNormalization() {
+	public function checkStringNormalizationDataProvider() {
 		$ascii_dejavu = 'dejavu';
-
-		$this->assertSame($ascii_dejavu, $this->fixture->normalize($ascii_dejavu, UnicodeNormalizer::NONE));
-		$this->assertSame($ascii_dejavu, $this->fixture->normalize($ascii_dejavu, UnicodeNormalizer::FORM_D));
-		$this->assertSame($ascii_dejavu, $this->fixture->normalize($ascii_dejavu, UnicodeNormalizer::FORM_KD));
-		$this->assertSame($ascii_dejavu, $this->fixture->normalize($ascii_dejavu, UnicodeNormalizer::FORM_C));
-		$this->assertSame($ascii_dejavu, $this->fixture->normalize($ascii_dejavu, UnicodeNormalizer::FORM_KC));
-	}
-
-	/**
-	 * Check if normalization works for pre-composed unicode-strings (only utf8)
-	 *
-	 * @test
-	 * @see http://forge.typo3.org/issues/57695
-	 */
-	public function checkPrecomposedStringNormalization() {
 		// fantasy-string: déjàvü
 		$nfc_dejavu = hex2bin('64c3a96ac3a076c3bc');
-
-		$this->assertSame($nfc_dejavu, $this->fixture->normalize($nfc_dejavu, UnicodeNormalizer::NONE));
-		$this->assertNotSame($nfc_dejavu, $this->fixture->normalize($nfc_dejavu, UnicodeNormalizer::FORM_D));
-		$this->assertNotSame($nfc_dejavu, $this->fixture->normalize($nfc_dejavu, UnicodeNormalizer::FORM_KD));
-		$this->assertSame($nfc_dejavu, $this->fixture->normalize($nfc_dejavu, UnicodeNormalizer::FORM_C));
-		$this->assertSame($nfc_dejavu, $this->fixture->normalize($nfc_dejavu, UnicodeNormalizer::FORM_KC));
-	}
-
-	/**
-	 * Check if normalization works for decomposed unicode-strings (only utf8)
-	 *
-	 * @test
-	 * @see http://forge.typo3.org/issues/57695
-	 */
-	public function checkDecomposedStringNormalization() {
 		// the same string as above, but decomposed
 		$nfd_dejavu = hex2bin('6465cc816a61cc807675cc88');
+		// combination of all three strings from above
+		$ascii_nfc_nfd_dejavu = $ascii_dejavu.$nfc_dejavu.$nfd_dejavu;
+		// the same as above, but already normalized to form D
+		$nfd_dejavu_triple = hex2bin('64656a6176756465cc816a61cc807675cc886465cc816a61cc807675cc88');
+		// the same as from two above, but already normalized to form C
+		$nfc_dejavu_triple = hex2bin('64656a61767564c3a96ac3a076c3bc64c3a96ac3a076c3bc');
 
-		$this->assertSame($nfd_dejavu, $this->fixture->normalize($nfd_dejavu, UnicodeNormalizer::NONE));
-		$this->assertSame($nfd_dejavu, $this->fixture->normalize($nfd_dejavu, UnicodeNormalizer::FORM_D));
-		$this->assertSame($nfd_dejavu, $this->fixture->normalize($nfd_dejavu, UnicodeNormalizer::FORM_KD));
-		$this->assertNotSame($nfd_dejavu, $this->fixture->normalize($nfd_dejavu, UnicodeNormalizer::FORM_C));
-		$this->assertNotSame($nfd_dejavu, $this->fixture->normalize($nfd_dejavu, UnicodeNormalizer::FORM_KC));
+		return array(
+			// pure ASCII string
+			'ASCII - normalized is identical if form is NONE' => array(
+				TRUE, $ascii_dejavu, UnicodeNormalizer::NONE
+			),
+			'ASCII - normalized to form D is identical' => array(
+				TRUE, $ascii_dejavu, UnicodeNormalizer::FORM_D
+			),
+			'ASCII - normalized to form DK is identical' => array(
+				TRUE, $ascii_dejavu, UnicodeNormalizer::FORM_KD
+			),
+			'ASCII - normalized to form C is identical' => array(
+				TRUE, $ascii_dejavu, UnicodeNormalizer::FORM_C
+			),
+			'ASCII - normalized to form KC is identical' => array(
+				TRUE, $ascii_dejavu, UnicodeNormalizer::FORM_KC
+			),
+			// unicode string in normalization-form C
+			'NFC - normalized is identical if form is NONE' => array(
+				TRUE, $nfc_dejavu, UnicodeNormalizer::NONE
+			),
+			'NFC - normalized to form D is different' => array(
+				FALSE, $nfc_dejavu, UnicodeNormalizer::FORM_D
+			),
+			'NFC - normalized to form DK is different' => array(
+				FALSE, $nfc_dejavu, UnicodeNormalizer::FORM_KD
+			),
+			'NFC - normalized to form C is identical' => array(
+				TRUE, $nfc_dejavu, UnicodeNormalizer::FORM_C
+			),
+			'NFC - normalized to form KC may be identical' => array(
+				TRUE, $nfc_dejavu, UnicodeNormalizer::FORM_KC
+			),
+			// unicode string in normalization-form D
+			'NFD - normalized is identical if form is NONE' => array(
+				TRUE, $nfd_dejavu, UnicodeNormalizer::NONE
+			),
+			'NFD - normalized to form D is identical' => array(
+				TRUE, $nfd_dejavu, UnicodeNormalizer::FORM_D
+			),
+			'NFD - normalized to form DK may be identical' => array(
+				TRUE, $nfd_dejavu, UnicodeNormalizer::FORM_KD
+			),
+			'NFD - normalized to form C is different' => array(
+				FALSE, $nfd_dejavu, UnicodeNormalizer::FORM_C
+			),
+			'NFD - normalized to form KC is different' => array(
+				FALSE, $nfd_dejavu, UnicodeNormalizer::FORM_KC
+			),
+			// combination of ascii and unicode strings in normalization-forms C and D
+			'ASCII + NFC + NFD - normalized is identical if form is NONE' => array(
+				$ascii_nfc_nfd_dejavu, $ascii_nfc_nfd_dejavu, UnicodeNormalizer::NONE
+			),
+			'ASCII + NFC + NFD - normalized to form D' => array(
+				$nfd_dejavu_triple, $ascii_nfc_nfd_dejavu, UnicodeNormalizer::NFD
+			),
+			'ASCII + NFC + NFD - normalized to form DK' => array(
+				$nfd_dejavu_triple, $ascii_nfc_nfd_dejavu, UnicodeNormalizer::NFKD
+			),
+			'ASCII + NFC + NFD - normalized to form C' => array(
+				$nfc_dejavu_triple, $ascii_nfc_nfd_dejavu, UnicodeNormalizer::NFC
+			),
+			'ASCII + NFC + NFD - normalized to form KC' => array(
+				$nfc_dejavu_triple, $ascii_nfc_nfd_dejavu, UnicodeNormalizer::NFKC
+			),
+		);
+
 	}
 
 	/**
-	 * Check if normalization works for combined ascii, pre- and decomposed unicode-strings (only utf8)
+	 * Check if unicode-normalization works for the provided types of strings
 	 *
 	 * @test
+	 * @dataProvider checkStringNormalizationDataProvider
 	 * @see http://forge.typo3.org/issues/57695
+	 *
+	 * @param boolean $expectedResult
+	 * @param string $testString
+	 * @param integer $normalizationForm
+	 * @return void
 	 */
-	public function checkCombinedStringNormalization() {
-		// wild combination of all three strings from above
-		$ascii_nfc_nfd_dejavu = 'dejavu'.hex2bin('64c3a96ac3a076c3bc').hex2bin('6465cc816a61cc807675cc88');
-		$this->assertSame($ascii_nfc_nfd_dejavu, $this->fixture->normalize($ascii_nfc_nfd_dejavu, UnicodeNormalizer::NONE));
-
-		$nfd_dejavu_triple = hex2bin('64656a6176756465cc816a61cc807675cc886465cc816a61cc807675cc88');
-		$this->assertSame($nfd_combined_dejavu, $this->fixture->normalize($nfd_dejavu_triple, UnicodeNormalizer::FORM_D));
-		$this->assertSame($nfd_combined_dejavu, $this->fixture->normalize($nfd_dejavu_triple, UnicodeNormalizer::FORM_KD));
-
-		$nfc_dejavu_triple = hex2bin('64656a61767564c3a96ac3a076c3bc64c3a96ac3a076c3bc');
-		$this->assertSame($nfc_combined_dejavu, $this->fixture->normalize($nfc_dejavu_triple, UnicodeNormalizer::FORM_C));
-		$this->assertSame($nfc_combined_dejavu, $this->fixture->normalize($nfc_dejavu_triple, UnicodeNormalizer::FORM_KC));
+	public function checkStringNormalization($expectedResult, $testString, $normalizationForm) {
+		$actualResult = $this->fixture->normalize($testString, $normalizationForm);
+		if ($expectedResult === TRUE) {
+			$this->assertSame($testString, $actualResult);
+		} elseif ($expectedResult === FALSE) {
+			$this->assertNotSame($testString, $actualResult);
+		} else {
+			$this->assertSame($expectedResult, $actualResult);
+		}
 	}
 }
